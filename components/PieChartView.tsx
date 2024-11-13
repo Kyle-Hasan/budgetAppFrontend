49e6b719eb9ec02a46eaf702d829e596260538c4
Kyle Hasan <kylehasan1@gmail.com>
2024-11-13 16:52:52 -0700

    add chart on web

--- components/PieChartView.tsx
+++ components/PieChartView.tsx
@@ -1,131 +1,122 @@
-
 import { Pie, PolarChart } from "victory-native";
-import React, { useEffect, useLayoutEffect, useMemo, useState } from 'react';
-import { View,Dimensions,Text, StyleSheet } from 'react-native';
-import { useFont } from "@shopify/react-native-skia";
-                            
-type pieChartDataFormat = {
-    name:string,
-    currentSpent:number,
-    color:string,
-    percent?:string
+import { Platform, View, Text, StyleSheet, Dimensions } from "react-native";
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie as WebPie } from 'react-chartjs-2';
+import "./pieChartWeb.css"
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+type PieChartDataFormat = {
+    name: string;
+    currentSpent: number;
+    color: string;
+    percent?: string;
+};
+
+interface PieChartViewProps {
+    pieChartData: PieChartDataFormat[];
 }
 
-interface PieChartViewComponents  {
-    pieChartData:pieChartDataFormat[],
+const PieChartView: React.FC<PieChartViewProps> = ({ pieChartData }) => {
+    const [data, setData] = useState<PieChartDataFormat[]>([]);
+
    
-}
-
-
-
-const PieChartView = ({pieChartData}:PieChartViewComponents)=> {
-
-    function getRandomColor(): string {
-        const minBrightness = 40;
+    const formatData = () => {
+        const temp = pieChartData.filter((x) => x.currentSpent > 0);
+        const totalSum = temp.reduce((total, current) => total + current.currentSpent, 0);
+        const colors = new Set<string>();
         
-        const red = Math.floor(Math.random() * (256 - minBrightness) + minBrightness);
-        const green = Math.floor(Math.random() * (256 - minBrightness) + minBrightness);
-        const blue = Math.floor(Math.random() * (256 - minBrightness) + minBrightness);
-      
-        // Convert RGB to hex
-        return `#${((1 << 24) + (red << 16) + (green << 8) + blue).toString(16).slice(1)}`;
-      }
-   
-
-    const [data,setData] = useState<pieChartDataFormat[]>([])
-
-    const formatData = ()=> {
-        const temp = [...pieChartData]
-        const totalSum = temp.reduce((total,current)=> {
-            return total+current.currentSpent 
-        },0)
-        //prevent identical colors
-        let colors = new Set<string>([])
-        console.log(temp)
-        setData(temp.filter(x=> x.currentSpent > 0).map(x=> {
-
-            let color = getRandomColor()
-            while(colors.has(color)) {
-                color = getRandomColor()
+        const formattedData = temp.map((x) => {
+            let color = getRandomColor();
+            while (colors.has(color)) {
+                color = getRandomColor();
             }
-            colors.add(color)
-            return {...x,percent:(x.currentSpent/totalSum * 100).toFixed(2),color}
-        }))
+            colors.add(color);
 
-    }
-    
+            return {
+                ...x,
+                percent: ((x.currentSpent / totalSum) * 100).toFixed(2),
+                color,
+            };
+        });
 
-    useLayoutEffect(()=>{
-        console.log("hi3")
-        formatData()
-    },[pieChartData])
+        setData(formattedData);
+    };
 
-    const [insetWidth, setInsetWidth] = useState(0);
-    const [insetColor, setInsetColor] = useState<string>("#ffffff");
-    const width = Dimensions.get('window').width * 0.90
-    const height = 10 * pieChartData.length
+    useEffect(() => {
+        formatData();
+    }, [pieChartData]);
+
+    const webData = {
+        labels: data.map((x) => x.name),
+        datasets: [
+            {
+                data: data.map((x) => x.currentSpent),
+                backgroundColor: data.map((x) => x.color || '#000'),
+            },
+        ],
+    };
+
+    const webOptions = {
+        plugins: {
+            legend: {
+                display: false, 
+            },
+        },
+    };
+
     return (
-        <View style={{ height:300}}>
-        <PolarChart
-            data={data}
-            colorKey={"color"}
-            valueKey={"currentSpent"}
-            labelKey={"name"}
-          >
-           <Pie.Chart>
-              {({ slice }) => {
-                return (
-                  <>
-                    <Pie.Slice>
-                     
-                        <Pie.Label  color={"white"} />
-                      
-                      
-                    </Pie.Slice>
+        <View style={{ height: 300 }}>
+            {Platform.OS === 'web' ? (
+                <WebPie data={webData} options={webOptions} className="chart" />
+            ) : (
+                <PolarChart
+                    data={data}
+                    colorKey="color"
+                    valueKey="currentSpent"
+                    labelKey="name"
+                >
+                    <Pie.Chart>
+                        {({ slice }) => (
+                            <>
+                                <Pie.Slice>
+                                    <Pie.Label color="white" />
+                                </Pie.Slice>
+                                <Pie.SliceAngularInset angularInset={{ angularStrokeWidth: 0,angularStrokeColor:"white" }} />
+                            </>
+                        )}
+                    </Pie.Chart>
+                </PolarChart>
+            )}
+            <View style={styles.legendContainer}>
+                {data.map((x) => (
+                    <View key={x.name} style={styles.labelContainer}>
+                        <View style={{ ...styles.box, backgroundColor: x.color }} />
+                        <Text style={styles.text}>
+                            {x.name} ({x.percent}%)
+                        </Text>
+                    </View>
+                ))}
+            </View>
+        </View>
+    );
+};
 
-                    <Pie.SliceAngularInset
-                      angularInset={{
-                        angularStrokeWidth: insetWidth,
-                        angularStrokeColor: insetColor,
-                      }}
-                    />
-                  </>
-                );
-              }}
-            </Pie.Chart>
-          </PolarChart>
-          <View style={styles.legendContainer}>
-            {data.map(x=> {
-                return <View key={x.name} style={styles.labelContainer} ><View style={{...styles.box,backgroundColor:x.color}}></View><Text style={styles.text}>{x.name}({x.percent}%)</Text></View>
-            })}
-          </View>
-      </View>
-    )
+// Utility function to generate random colors
+function getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
+    return color;
 }
 
 const styles = StyleSheet.create({
-    text: {
-        color:"white"
-    },
-    box: {
-        width:20,
-        height:20
-    }, 
-    legendContainer: {
-        display:"flex",
-        marginBottom:2,
-        marginLeft:10
-    },
-    labelContainer: {
-    
-        display:"flex",
-        flexDirection:"row",
-        gap:2,
-        marginBottom:2
-    }
-})
+    text: { color: "white" },
+    box: { width: 20, height: 20 },
+    legendContainer: { display: "flex", marginBottom: 2, marginLeft: Platform.OS == "web" ? "35%" : 10 },
+    labelContainer: { display: "flex", flexDirection: "row", gap: 2, marginBottom: 2 },
+});
 
 export default React.memo(PieChartView, (prevProps, nextProps) => {
-    return prevProps.pieChartData.length == nextProps.pieChartData.length;
-  });
-  
+    return prevProps.pieChartData.length === nextProps.pieChartData.length;
+});
